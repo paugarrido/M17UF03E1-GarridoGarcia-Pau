19e63478bc703f0a59cff222cb9dcbcbd424e413
paugarrido <pau.garrido.7e3@itb.cat>
2021-02-14 22:55:59 +0100

    Segundo commit: Entrega Final

---
+++ StandardAssets/Assets/Scripts/FinishCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishCoin : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
--- StandardAssets/Assets/Scripts/PlatformMovement.cs
+++ StandardAssets/Assets/Scripts/PlatformMovement.cs
@@ -9,6 +9,7 @@ public class PlatformMovement : MonoBehaviour
     public KeyCode keyDown;
     public KeyCode keyLeft;
     public KeyCode KeyRight;
+    public Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,23 @@ public class PlatformMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(keyLeft))
+        {
+            transform.RotateAround(center, Vector3.forward, 20 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyRight))
+        {
+            transform.RotateAround(center, -Vector3.forward, 20 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(keyUp))
+        {
+            transform.RotateAround(center, Vector3.right, 20 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(keyDown))
+        {
+            transform.RotateAround(center, Vector3.left, 20 * Time.deltaTime);
+        }
     }
 }
